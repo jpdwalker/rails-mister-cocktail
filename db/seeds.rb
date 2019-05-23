cf84c0b6d468ca1219d76dc6ae71621b9949fef8
Joe Walker <jpdwalker@gmail.com>
2019-05-23 16:48:58 +0800

    finalised backend logic of app

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,26 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Dose.destroy_all
+Ingredient.destroy_all
+Cocktail.destroy_all
+
+puts "creating three ingredients"
+ingredient_one = Ingredient.create(name: "ice")
+ingredient_two = Ingredient.create(name: "mint leaves")
+ingredient_three = Ingredient.create(name: "lemon")
+
+ingredients = [ingredient_one, ingredient_two, ingredient_three]
+
+puts "creating cocktails and doses "
+5.times do
+  cocktail = Cocktail.create!(name: Faker::Cannabis.cannabinoid )
+  5.times do
+    Dose.create!(
+      description: Faker::Cannabis.buzzword,
+      cocktail: cocktail,
+      ingredient: ingredients.sample
+    )
+  end
+end
