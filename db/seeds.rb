3f4ec0f23f34ce5c179548d65cd1f1b9f4ad5332
Joe Walker <jpdwalker@gmail.com>
2019-05-24 11:52:51 +0800

    latest version

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,25 +6,40 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
+# Dose.destroy_all
+# Ingredient.destroy_all
+# Cocktail.destroy_all
+
+# puts "creating three ingredients"
+# ingredient_one = Ingredient.create(name: "ice")
+# ingredient_two = Ingredient.create(name: "mint leaves")
+# ingredient_three = Ingredient.create(name: "lemon")
+
+# ingredients = [ingredient_one, ingredient_two, ingredient_three]
+
+# puts "creating cocktails and doses "
+# 5.times do
+#   cocktail = Cocktail.create!(name: Faker::Cannabis.cannabinoid )
+#   5.times do
+#     Dose.create!(
+#       description: Faker::Cannabis.buzzword,
+#       cocktail: cocktail,
+#       ingredient: ingredients.sample
+#     )
+#   end
+# end
+
+require 'open-uri'
+require 'json'
+
+Cocktail.destroy_all
 Dose.destroy_all
 Ingredient.destroy_all
-Cocktail.destroy_all
+url = "https://www.thecocktaildb.com/api/json/v1/1/list.php?i=list"
 
-puts "creating three ingredients"
-ingredient_one = Ingredient.create(name: "ice")
-ingredient_two = Ingredient.create(name: "mint leaves")
-ingredient_three = Ingredient.create(name: "lemon")
+ingredient_serialized = open(url).read
+ingredients = JSON.parse(ingredient_serialized)
 
-ingredients = [ingredient_one, ingredient_two, ingredient_three]
-
-puts "creating cocktails and doses "
-5.times do
-  cocktail = Cocktail.create!(name: Faker::Cannabis.cannabinoid )
-  5.times do
-    Dose.create!(
-      description: Faker::Cannabis.buzzword,
-      cocktail: cocktail,
-      ingredient: ingredients.sample
-    )
-  end
+ingredients["drinks"].each do |ingredient|
+  Ingredient.create(name: ingredient["strIngredient1"])
 end
